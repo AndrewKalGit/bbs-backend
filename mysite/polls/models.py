e003500c7208d332d3ed724d89ff276498e71b94
Andrew Kal <andrewkalgit@gmail.com>
2022-09-09 21:33:24 -0400

    initilize backend

---
+++ mysite/polls/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class YearsServing(models.Model):
+    years_serving_integer = models.IntegerField()
+
+
+class Members(models.Model):
+    members_integer = models.IntegerField()
